d70cd2d05cd3f5cbbe8dd791ee6fbcc23fac2d50
cmot17 <25753364+cmot17@users.noreply.github.com>
2023-09-08 14:31:01 -0400

    make it drive fr fr ong this time i think

--- warmup_project/warmup_project/drive_test.py
+++ warmup_project/warmup_project/drive_test.py
@@ -4,16 +4,17 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import Vector3
 
 class DriveTest(Node):
-    def __init__(self):
-        super().__init__('DriveTest')
+    def __init__(self) -> None:
+        super().__init__('DriveTest') # type: ignore
         self.neato_pub: Publisher = self.create_publisher(Twist, "cmd_vel", 10)
         self.create_timer(0.05, self.loop)
 
-    def loop(self):
+    def loop(self) -> None:
+        print("among us")
         msg: Twist = Twist()
-        msg.angular: Vector3 = Vector3(x=1.0, y=0.0, z=0.0)
-        forward_vel: float = 1.0
-        msg.linear: Vector3 = Vector3(x=forward_vel, y=0.0, z=0.0) 
+        msg.angular = Vector3(x=0.0, y=0.0, z=0.0)
+        forward_vel = 0.1
+        msg.linear = Vector3(x=forward_vel, y=0.0, z=0.0)
         self.neato_pub.publish(msg)
 
 
