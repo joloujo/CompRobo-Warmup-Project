14538d36ec1a5ebdaf25babc1c26e8d30b2ab978
Dominic Salmieri <dsalmieri@olin.edu>
2023-09-12 02:16:18 -0400

    ben why did you build in the repo bro?

--- build/warmup_project/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/bentarr/ros2_ws/src/CompRobo-Warmup-Project/install/warmup_project'
--- build/warmup_project/share/warmup_project/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/bentarr/ros2_ws/src/CompRobo-Warmup-Project/build/warmup_project"
