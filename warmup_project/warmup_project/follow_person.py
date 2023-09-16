6d40be79d2b35d95c0eb12d987b0b14084feb8d9
Dominic Salmieri <dsalmieri@olin.edu>
2023-09-16 17:18:50 -0400

    Cleaned up a bit and made person following work

--- warmup_project/warmup_project/follow_person.py
+++ warmup_project/warmup_project/follow_person.py
@@ -1,46 +1,45 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist, PoseArray, Pose, PointStamped
-from sensor_msgs.msg import LaserScan, PointCloud2
+from sensor_msgs.msg import LaserScan
 import numpy as np
 
 class FollowPerson(Node):
-    detection_width = 1
-    detection_min_dist = -1
+    detection_width = 2
+    detection_min_dist = 2 * -1
     detection_max_dist = 0
 
     def __init__(self):
         super().__init__('follow_person') # type: ignore
+        # create publisher to control the neato
         self.neato_pub = self.create_publisher(Twist, "cmd_vel", 10)
-        self.scan_sub = self.create_subscription(LaserScan, "stable_scan", self.scan_callback, 10)
-        self.create_timer(0.01, self.loop)
-
+        # create publisher to publish point cloud
         self.point_cloud_pub = self.create_publisher(PoseArray, "point_cloud", 10)
+        # create publisher to publish center of mass
         self.COM_pub = self.create_publisher(PointStamped, "center_of_mass", 10)
+        # create subscriber to get laser scan data
+        self.scan_sub = self.create_subscription(LaserScan, "stable_scan", self.scan_callback, 10)
 
+        # create timer to run loop
+        self.create_timer(0.01, self.loop)
+        
     def scan_callback(self, scan):
         points_x, points_y = self.process_scan(scan)
         self.publish_point_cloud(points_x, points_y)
 
         # ---------- Calculate center of mass ----------
+        # if no points are in the detection box, stop the neato
+        if len(points_x) == 0:
+            self.drive_neato(0.0, 0.0)
+            return
+        
+        # if there are points in the detection box, drive the neato towards the center of mass
         center_x = np.mean(points_x)
         center_y = np.mean(points_y)
 
-        # DEBUG print center of mass
-        # print(center_x, center_y)
+        self.publish_COM(center_x, center_y)
 
-        # create message
-        msg = PointStamped()
-        msg.header.frame_id = "base_laser_link"
-
-        # add center of mass to message
-        msg.point.x = center_x
-        msg.point.y = center_y
-
-        print(msg)
-
-        # publish message
-        self.COM_pub.publish(msg=msg)
+        self.control(center_x, center_y)
 
     def process_scan(self, scan):
         # get raw data into numpy arrays
@@ -99,15 +98,40 @@ class FollowPerson(Node):
 
     def publish_COM(self, center_x, center_y):
         # create message
-        msg = Pose()
+        msg = PointStamped()
+        msg.header.frame_id = "base_laser_link"
 
         # add center of mass to message
-        msg.position.x = center_x
-        msg.position.y = center_y
+        msg.point.x = center_x
+        msg.point.y = center_y
+
+        # DEBUG print message
+        # print(msg)
 
         # publish message
         self.COM_pub.publish(msg=msg)
 
+    def control(self, target_x, target_y):
+        # calculate distance and angle to target
+        distance = np.sqrt(target_x**2 + target_y**2)
+        angle = np.arctan2(target_y, target_x)
+
+        command_linear = np.clip((distance-0.3)/2, -0.1, 0.3)
+        command_angular = np.clip(angle * -0.5, -1, 1)
+
+        # drive towards target
+        self.drive_neato(linear=command_linear, angular=command_angular)
+
+    def drive_neato(self, linear: float, angular: float):
+        # create message
+        msg = Twist()
+
+        # add speed and angle to message
+        msg.linear.x = linear
+        msg.angular.z = angular
+
+        # publish message
+        self.neato_pub.publish(msg=msg)
 
     def loop(self):
         pass
